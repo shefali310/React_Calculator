4c4648938c0ef826b7934e0c84ee164d299f9a71
Shefali Panchal <61265165+shefali310@users.noreply.github.com>
2024-05-16 00:38:40 -0400

    result generated

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,32 @@
+import Header from "./components/Header.jsx"
+import Results from "./components/Results.jsx";
+import UserInput from "./components/UserInput.jsx"
+import { useState } from "react";
+
 function App() {
+  const [userInput, setUserInput] = useState({
+    initialInvestment: 10000,
+    annualInvestment: 1200,
+    expectedReturn: 6,
+    duration: 10,
+  });
+
+  function handleChange(inputIdentifier, newValue) {
+    setUserInput((prevUserInput) => {
+      return {
+        ...prevUserInput,
+        [inputIdentifier]: + newValue,
+      };
+    });
+  }
   return (
-    <h1>React Investment Calculator</h1>
+    <>
+     <Header/>
+    <UserInput userInput={userInput} onChange={handleChange} />
+   
+    <Results input={userInput}/>
+    </>
+   
   )
 }
 
